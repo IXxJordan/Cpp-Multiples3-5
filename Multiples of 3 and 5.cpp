b2d3e382b8772ef71b52efcb6ed4009f1f1e5a84
~ Iòrdan ~ <ixxjordan@hotmail.co.uk>
2021-01-30 06:57:30 +0000

    Add project files.

---
+++ Multiples of 3 and 5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+int sumMultiples(int n) 
+{
+    int result = 0, temp = 0;
+    std::cout << "Multiples of 3 or 5 below " << n << ":\n";
+
+    for (int i = 1; i < n; i++) 
+    {
+        if (i%3 == 0 || i%5 == 0)
+        {
+            std::cout << i << " ";
+            result += i;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    std::cout << "Project Euler: Problem 1 - Multiples of 3 and 5.\n\n";
+
+    std::cout << "\nSum of Multiples: " << sumMultiples(1000) << "\n\n";
+}
+
+/*
+* Problem:
+* If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+* Find the sum of all the multiples of 3 or 5 below 1000.
+*/
